9d055d2e314ee55347a123de92be5b042ef40f6e
Henderson The Coder School <68570781+hendersoncoderschool@users.noreply.github.com>
2024-08-07 19:00:35 -0700

    aim rotaion fix

--- Assets/AimControllerJENGA.cs
+++ Assets/AimControllerJENGA.cs
@@ -9,6 +9,9 @@ public class AimControllerJENGA : MonoBehaviour
     public float speed;
     public GameObject v;
     public float offset;
+    public float verticalRotationLimit = 50f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,18 +21,25 @@ public class AimControllerJENGA : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        vertical = Input.GetAxis("Mouse Y");
-        transform.Rotate(Vector3.right, -vertical * speed * Time.deltaTime);
-        horizontal = Input.GetAxis("Mouse X");
-        transform.Rotate(Vector3.up, horizontal * speed * Time.deltaTime);
+        float horizontalInput = Input.GetAxis("Mouse X");
+        transform.Rotate(Vector3.up, horizontalInput * speed * Time.deltaTime);
 
+        float verticalInput = Input.GetAxis("Mouse Y");
+        vertical -= verticalInput * speed * Time.deltaTime;
+        vertical = Mathf.Clamp(vertical, -verticalRotationLimit, verticalRotationLimit);
+
+        transform.localRotation = Quaternion.Euler(vertical, transform.localEulerAngles.y, 0f);
 
         if (Input.GetButtonDown("Fire1"))
         {
             GameObject projectile = (GameObject) Instantiate(v, transform.position + transform.forward * offset, Quaternion.identity);
             Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
-            projectileRb.AddForce(20f * Vector3.forward, ForceMode.Impulse);
+            projectileRb.AddForce(20f * transform.forward, ForceMode.Impulse);
             
         }
     }
 }
+//vertical = Input.GetAxis("Mouse Y");
+        //transform.Rotate(Vector3.right, -vertical * speed * Time.deltaTime);
+        //horizontal = Input.GetAxis("Mouse X");
+        //transform.Rotate(Vector3.up, horizontal * speed * Time.deltaTime);
