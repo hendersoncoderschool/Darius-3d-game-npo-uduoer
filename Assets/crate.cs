2fe3372ef2a2162d3c89e1398430d1c33e74a4a4
Henderson The Coder School <hendersoncoderschool@gmail.com>
2024-08-21 18:59:44 -0700

    crate collisions

--- Assets/crate.cs
+++ Assets/crate.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class crate : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
+
+
+
+
+
     void Update()
     {
-        
+
     }
+    void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("Rock"))
+            {
+            Destroy(gameObject);
+            print("hellO");
+        
+            }
+    
+        }
+
 }
